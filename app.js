d5ea6bf53a480fb8b35041df814a29e1e2e1dcf5
kkinge79 <kingerykagan@gmail.com>
2021-10-01 21:42:58 -0500

    finish step 2

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+
+
+
+
 const game = {
   title: 'Guess the Number!',
+  prevGuesses: [],
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  getGuess() {
+    prompt ('Enter a guess between ${smallestNum} and ${biggestNum}')
+  },
+  
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   }
 }
+console.log(game.getGuess())
